a2b9988606338e311cb03ce11b96048bcbdbc6d3
kamyar taeb <kamyar.taeb@gmail.com>
2023-06-02 01:55:51 +0330

    sevice3 changes

--- service3/client.go
+++ service3/client.go
@@ -4,40 +4,107 @@ import (
 	"context"
 	"log"
 	"math/rand"
+	"time"
 
+	pb "github.com/royadaneshi/webHW1/service3/authservice" // Update with the correct package path
 	"google.golang.org/grpc"
-
-	pb "github.com/royadaneshi/webHW1/royadaneshi/webHW1/service3/authservice" // Update with the correct package path
 )
 
 func main() {
-	// Set up a connection to the gRPC server
-	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure())
+	// Set up a connection to the server
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
 	defer conn.Close()
 
-	// Create a new gRPC client
+	// Create a new client
 	client := pb.NewMyServiceClient(conn)
 
-	// Prepare the request
-	request := &pb.MyRequest{
-		MessageId: 1234, 
-		Nonce: string(func(l int) []byte {
-			b := make([]byte, l)
-			for i := range b {
-				b[i] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"[rand.Intn(len("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"))]
-			}
-			return b
-		}(rand.Intn(20) + 1)), 
-	}
+	// Generate a random even number
+	messageID := generateEvenNumber()
 
-	// Send the request to the server
-	response, err := client.ProcessRequest(context.Background(), request)
+	// Generate a random nonce string
+	nonce := generateRandomString(20)
+
+	// Create a context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// Send the gRPC request to the server
+	response, err := client.MyMethod(ctx, &pb.MyRequest{MessageId: messageID, Nonce: nonce})
 	if err != nil {
-		log.Fatalf("Failed to process request: %v", err)
+		log.Fatalf("Failed to call MyMethod: %v", err)
 	}
 
-	log.Printf("Response: %v", response)
+	// Print the response
+	log.Printf("Nonce sent by client: %s", response.Nonce)
+	log.Printf("Server Nonce: %s", response.ServerNonce)
+	log.Printf("Message: %d", response.Message)
+	log.Printf("P: %d", response.P)
+	log.Printf("G: %d", response.G)
 }
+
+// Generate a random even number greater than 0
+func generateEvenNumber() int32 {
+	for {
+		num := rand.Int31n(1000) + 1 // Generate a random number between 1 and 1000
+		if num%2 == 0 {
+			return num
+		}
+	}
+}
+
+// Generate a random string of the specified length
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	var result string
+	for i := 0; i < length; i++ {
+		result += string(charset[rand.Intn(len(charset))])
+	}
+	return result
+}
+
+// package main
+
+// import (
+// 	"context"
+// 	"log"
+// 	"math/rand"
+
+// 	"google.golang.org/grpc"
+
+// 	pb "github.com/royadaneshi/webHW1/royadaneshi/webHW1/service3/authservice" // Update with the correct package path
+// )
+
+// func main() {
+// 	// Set up a connection to the gRPC server
+// 	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure())
+// 	if err != nil {
+// 		log.Fatalf("Failed to connect: %v", err)
+// 	}
+// 	defer conn.Close()
+
+// 	// Create a new gRPC client
+// 	client := pb.NewMyServiceClient(conn)
+
+// 	// Prepare the request
+// 	request := &pb.MyRequest{
+// 		MessageId: 1234,
+// 		Nonce: string(func(l int) []byte {
+// 			b := make([]byte, l)
+// 			for i := range b {
+// 				b[i] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"[rand.Intn(len("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"))]
+// 			}
+// 			return b
+// 		}(rand.Intn(20) + 1)),
+// 	}
+
+// 	// Send the request to the server
+// 	response, err := client.ProcessRequest(context.Background(), request)
+// 	if err != nil {
+// 		log.Fatalf("Failed to process request: %v", err)
+// 	}
+
+// 	log.Printf("Response: %v", response)
+// }
--- service3/req_pq.go
+++ service3/req_pq.go
@@ -2,51 +2,144 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"crypto/rand"
+	"crypto/sha1"
+	"encoding/hex"
 	"log"
+	"math/big"
+	mrand "math/rand"
 	"net"
+	"time"
 
-	"google.golang.org/grpc"
+	"github.com/go-redis/redis"
 
 	pb "github.com/royadaneshi/webHW1/service3/authservice" // Update with the correct package path
+	"google.golang.org/grpc"
 )
 
-type myServiceServer struct{}
-
-func (s *myServiceServer) ProcessRequest(ctx context.Context, req *pb.MyRequest) (*pb.MyResponse, error) {
-	// Validate the input
-	if req.MessageId%2 != 0 || req.MessageId <= 0 {
-		return nil, fmt.Errorf("Invalid message ID")
-	}
-	if len(req.Nonce) != 20 {
-		return nil, fmt.Errorf("Invalid nonce length")
-	}
-
-	// Process the request
-	// Replace the following code with your own implementation
-	response := &pb.MyResponse{
-		// Set your response message here
-	}
-
-	return response, nil
+type server struct {
+	pb.UnimplementedMyServiceServer
+	redisClient *redis.Client
 }
 
 func main() {
-	// Create a TCP listener on port 50052
-	lis, err := net.Listen("tcp", ":50052")
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
-	}
+	// Connect to Redis
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // Update with your Redis server address
+		Password: "",               // Set your Redis server password (if any)
+		DB:       0,                // Set the Redis database index
+	})
+	defer redisClient.Close()
+
+	// Initialize the random number generator
+	mrand.Seed(time.Now().UnixNano())
 
 	// Create a new gRPC server
 	grpcServer := grpc.NewServer()
+	s := &server{redisClient: redisClient}
+	pb.RegisterMyServiceServer(grpcServer, s)
 
-	// Register the service implementation
-	pb.RegisterMyServiceServer(grpcServer, &myServiceServer{})
-
-	// Start the server
-	log.Println("Starting gRPC server on port 50051...")
+	// Start the gRPC server
+	lis := listenGRPC()
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
+		log.Fatalf("Failed to start gRPC server: %v", err)
 	}
+}
+
+func (s *server) MyMethod(ctx context.Context, req *pb.MyRequest) (*pb.MyResponse, error) {
+	// Generate a random nonce string
+	nonce := generateRandomString(20)
+
+	p, g := generatePrimeAndPrimitiveRoot()
+
+	message := generateOddNumber()
+
+	key := generateSHA1Hash(nonce + req.Nonce)
+
+	if err := s.setRedisValue(key, nonce, time.Hour); err != nil {
+		log.Printf("Failed to store data in Redis: %v", err)
+	}
+
+	resp := &pb.MyResponse{
+		Nonce:       req.Nonce,
+		ServerNonce: nonce,
+		Message:     message,
+		P:           p,
+		G:           g,
+	}
+	return resp, nil
+}
+
+func generateOddNumber() int32 {
+	for {
+		num := mrand.Int31n(1000) + 1
+		if num%2 != 0 {
+			return num
+		}
+	}
+}
+
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	var result string
+	for i := 0; i < length; i++ {
+		result += string(charset[mrand.Intn(len(charset))])
+	}
+	return result
+}
+
+func generatePrimeAndPrimitiveRoot() (int32, int32) {
+	p, err := rand.Prime(rand.Reader, 128)
+	if err != nil {
+		log.Fatalf("Failed to generate prime number: %v", err)
+	}
+
+	g := generatePrimitiveRoot(p)
+
+	return int32(p.Int64()), int32(g.Int64())
+}
+
+func generateSHA1Hash(input string) string {
+	hash := sha1.Sum([]byte(input))
+	return hex.EncodeToString(hash[:])
+}
+
+func (s *server) setRedisValue(key string, value interface{}, expiration time.Duration) error {
+	return s.redisClient.Set(key, value, expiration).Err()
+}
+
+func generatePrimitiveRoot(p *big.Int) *big.Int {
+	if !p.ProbablyPrime(10) {
+		log.Fatalf("Input number is not prime")
+	}
+
+	for i := big.NewInt(2); i.Cmp(p) < 0; i.Add(i, big.NewInt(1)) {
+		if isPrimitiveRoot(i, p) {
+			return i
+		}
+	}
+
+	log.Fatalf("Failed to find a primitive root")
+	return nil
+}
+
+func isPrimitiveRoot(g, p *big.Int) bool {
+	one := big.NewInt(1)
+	pMinusOne := new(big.Int).Sub(p, one)
+
+	exp := new(big.Int).Div(pMinusOne, big.NewInt(2))
+
+	modPow := new(big.Int).Exp(g, exp, p)
+
+	return modPow.Cmp(one) != 0
+}
+
+// Listen for gRPC connections
+func listenGRPC() net.Listener {
+	lis, err := net.Listen("tcp", ":50051") // Update with your desired server address
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	log.Println("gRPC server listening on port 50051")
+	return lis
 }
